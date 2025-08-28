0a6b4901cec02ea4d007eaefa0059970cd9eba01
sholehbaktiabadi <sholehbaktiabadi@gmail.com>
2025-08-28 20:23:01 +0700

    chore: add app logos on sent otp page

--- app/sent-otp.tsx
+++ app/sent-otp.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { View, Text, Alert, Pressable } from 'react-native';
+import { View, Text, Alert, Pressable, Image } from 'react-native';
 import { router } from 'expo-router';
 import LottieView from 'lottie-react-native';
 import { isValidNumber, PhoneInput } from 'react-native-phone-entry';
@@ -38,53 +38,53 @@ export default function SentOtp() {
 
     return (
         <>
-        <LinearGradient
-          colors={['#ff792cff', '#ffb387ff', '#ffcaabff', '#ffeee5ff']}
-          locations={[0.1, 0.39, 0.4, 1]}
-          className="absolute top-0 left-0 right-0 bottom-0"
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 1 }}
-        >
-            <View style={{ flex: 1, justifyContent: 'center', padding: 40 }}>
-                <Text className='text-gray-100 mb-20 text-center text-3xl font-sans'>
-                    Kirim Kode OTP
-                </Text>
-                <LottieView
-                    autoPlay
-                    speed={2}
-                    style={{
-                        width: 340,
-                        height: 150
-                    }}
-                    source={require("../assets/lottie/verification.json")}
-                />
-                <PhoneInput
-                    defaultValues={{
-                        countryCode: 'ID',
-                        callingCode: '+62',
-                        phoneNumber: '+62',
-                    }}
-                    countryPickerProps={{ disableNativeModal: true, countryCode: "ID", onSelect: (() => console.log()) }}
-                    isCallingCodeEditable={true}
-                    hideDropdownIcon={true}
-                    maskInputProps={{ mask: [/\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/] }}
-                    onChangeText={(text) => {
-                        console.log(
-                            'isValidNumber:',
-                            isValidNumber(text, "ID"),
-                        )
-                        setPhone(text)
-                    }
-                    }
-                />
-                <View className="items-center">
-                    <Pressable
-                        className="mt-5 items-center rounded-xl border border-orange-400 bg-orange-400 shadow shadow-slate-700 w-[70%]"
-                        onPress={async () => await handleLogin()}>
-                        <Text className="m-3 font-bold text-white">{loading ? "Mengirim..." : "Kirim Otp"}</Text>
-                    </Pressable>
+            <LinearGradient
+                colors={['#ff792cff', '#ffb387ff', '#ffcaabff', '#ffeee5ff']}
+                locations={[0.1, 0.39, 0.4, 1]}
+                className="absolute top-0 left-0 right-0 bottom-0"
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
+            >
+                <View style={{ flex: 1, justifyContent: 'center', padding: 40 }}>
+                    <Text className='text-white mb-1 text-3xl font-sans font-extrabold'>
+                        Kirim Kode OTP
+                    </Text>
+                    <Text className='text-gray-100 mb-10 font-sans font-extrabold'>
+                        Masukan nomor whatsapp
+                    </Text>
+                    <View className='mb-10 mx-auto'>
+                        <Image
+                            className="h-32 w-32 rounded-2xl"
+                            source={require('../assets/app/kliniku.png')}
+                        />
+                    </View>
+                    <PhoneInput
+                        defaultValues={{
+                            countryCode: 'ID',
+                            callingCode: '+62',
+                            phoneNumber: '+62',
+                        }}
+                        countryPickerProps={{ disableNativeModal: true, countryCode: "ID", onSelect: (() => console.log()) }}
+                        isCallingCodeEditable={true}
+                        hideDropdownIcon={true}
+                        maskInputProps={{ mask: [/\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/] }}
+                        onChangeText={(text) => {
+                            console.log(
+                                'isValidNumber:',
+                                isValidNumber(text, "ID"),
+                            )
+                            setPhone(text)
+                        }
+                        }
+                    />
+                    <View className="items-center">
+                        <Pressable
+                            className="mt-5 items-center rounded-xl border border-orange-400 bg-orange-400 shadow shadow-slate-700 w-[70%]"
+                            onPress={async () => await handleLogin()}>
+                            <Text className="m-3 font-bold text-white">{loading ? "Mengirim..." : "Kirim Otp"}</Text>
+                        </Pressable>
+                    </View>
                 </View>
-            </View>
             </LinearGradient>
         </>
     );
